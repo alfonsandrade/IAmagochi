3eefafda1339cd107901bd69fcb5d33ec4fdaa9f
VitorCaversan <joaopassos@alunos.utfpr.edu.br>
2024-11-24 20:34:57 -0300

    Adds the sleep time option.

--- js/calendar.js
+++ js/calendar.js
@@ -9,39 +9,67 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // CALENDAR CONFIG QUESTIONS
     const cfgBtn = document.getElementById('calendar-config-button');
-    const popup = document.getElementById('popup');
-    const closeButton = document.querySelector('.close-popup');
-    const popupOptions = document.querySelectorAll('.popup-option');
+    const studyTimePopup = document.getElementById('studyTimePopup');
+    const studyTimeCloseBtn = studyTimePopup.querySelector('.close-popup');
+    const studyTimePopupOpts = studyTimePopup.querySelectorAll('.popup-option');
+
+    const sleepTimePopup = document.getElementById('sleepTimePopup');
+    const sleepTimeCloseBtn = sleepTimePopup.querySelector('.close-popup');
+    const sleepTimePopupOpts = sleepTimePopup.querySelectorAll('.popup-option');
 
     cfgBtn.addEventListener('click', function() {
-        popup.style.display = 'block';
+        studyTimePopup.style.display = 'block';
     });
 
-    closeButton.addEventListener('click', function() {
-        popup.style.display = 'none';
+    studyTimeCloseBtn.addEventListener('click', function() {
+        studyTimePopup.style.display = 'none';
     });
 
     window.addEventListener('click', function(event) {
-        if (event.target == popup) {
-            popup.style.display = 'none';
+        if (event.target == studyTimePopup) {
+            studyTimePopup.style.display = 'none';
+        }
+        else if (event.target == sleepTimePopup) {
+            sleepTimePopup.style.display = 'none';
         }
     });
 
     let answer = '';
-    popupOptions.forEach(option => {
+    studyTimePopupOpts.forEach(option => {
         option.addEventListener('click', function() {
             answer = this.getAttribute('data-answer');
             console.log('Answer:', answer);
-            popup.style.display = 'none';
+            studyTimePopup.style.display = 'none';
+            sleepTimePopup.style.display = 'block';
+        });
+    });
+
+    sleepTimeCloseBtn.addEventListener('click', function() {
+        sleepTimePopup.style.display = 'none';
+    });
+
+    sleepTimePopupOpts.forEach(option => {
+        option.addEventListener('click', function() {
+            answer = answer + '_' + this.getAttribute('data-answer');
+            console.log('Answer:', answer);
+            sleepTimePopup.style.display = 'none';
             fillCalendar(answer);
         });
     });
 
     function fillCalendar(answer) {
+        const [studyTimeAnswer, sleepTimeAnswer] = answer.split('_');
+        console.log('Study Time Answer:', studyTimeAnswer);
+        console.log('Sleep Time Answer:', sleepTimeAnswer);
+
         const occupiedHours = {
             'Morning': [8, 9, 10, 11],
             'Afternoon': [12, 13, 14, 15],
-            'Morning-Afternoon': [7, 8, 9, 10, 12, 13, 14, 15]
+            'Morning-Afternoon': [7, 8, 9, 10, 12, 13, 14, 15],
+            '19h': [19, 20, 21, 22, 23, 0, 1, 2, 3, 4, 5],
+            '20h': [20, 21, 22, 23, 0, 1, 2, 3, 4, 5],
+            '21h': [21, 22, 23, 0, 1, 2, 3, 4, 5],
+            '22h': [22, 23, 0, 1, 2, 3, 4, 5],
         };
 
         const days = document.querySelectorAll('.day');
@@ -53,7 +81,8 @@ document.addEventListener("DOMContentLoaded", () => {
                 const hourBlock = document.createElement('div');
                 hourBlock.classList.add('hour-block');
                 hourBlock.textContent = `${hour}:00`;
-                if (occupiedHours[answer].includes(hour)) {
+                if (occupiedHours[studyTimeAnswer].includes(hour) ||
+                    occupiedHours[sleepTimeAnswer].includes(hour)) {
                     hourBlock.classList.add('occupied');
                 }
                 hoursContainer.appendChild(hourBlock);
