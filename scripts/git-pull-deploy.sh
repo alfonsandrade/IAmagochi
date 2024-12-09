ed54529f4cfd13d23d42cdb126ae0100240c7e9d
Alfons Andrade <alfons@alunos.utfpr.edu.br>
2024-12-08 23:21:26 -0300

    fast

--- scripts/git-pull-deploy.sh
+++ scripts/git-pull-deploy.sh
@@ -7,8 +7,14 @@ ssh-add /home/alfons/.ssh/id_ed25519
 # Navigate to the project directory
 cd /home/alfons/git/IAmagochi || exit
 
-# Reset any local changes and pull the latest changes from the default branch
+# Discard any local changes to avoid issues
 git reset --hard HEAD
+
+# Ensure the latest state of the branch is fetched
+git clean -fd
+git fetch origin
+
+# Pull the latest changes from the repository
 git pull origin $(git rev-parse --abbrev-ref HEAD)
 
 # Set permissions for Apache to access the files (if necessary)
